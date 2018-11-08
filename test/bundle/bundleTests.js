537e9dffdabc978d220f889b54baa088678f46ca
Golo Roden <golo.roden@thenativeweb.io>
2018-11-08 16:35:29 +0100

    Update bundle tests.

--- test/bundle/bundleTests.js
+++ test/bundle/bundleTests.js
@@ -8,11 +8,11 @@ const assert = require('assertthat');
 const pathToBundle = path.join(__dirname, '..', '..', 'bundle', 'wolkenkitClient.js');
 
 suite('bundle', () => {
-  test('should be below 200kb.', async () => {
+  test('should be below 225kb.', async () => {
     /* eslint-disable */
     const bundleStats = fs.statSync(pathToBundle);
     /* eslint-enable */
 
-    assert.that(bundleStats.size / 1024).is.atMost(200);
+    assert.that(bundleStats.size / 1024).is.atMost(225);
   });
 });
